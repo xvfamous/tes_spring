9b90eb99da644a087fdeb436c3951edc9cd1992a
Risky Leonardo <58988510+xvfamous@users.noreply.github.com>
2021-12-21 19:45:04 +0700

    Update productRepository.java

--- demo-api/src/main/java/com/farma/models/repos/productRepository.java
+++ demo-api/src/main/java/com/farma/models/repos/productRepository.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface productRepository extends JpaRepository<Product, Long> {
-	List<Product> findByStatusPeminjaman (int statusPembelian);
-	List<Product> findByTitleBook(String nama_barang);
+	List<Product> findByStatusPembelian (int statusPembelian);
+	List<Product> findBynama_barang (String nama_barang);
     Product findOne(Long id);
     List<Product> findBynama_barang(String nama_barang);
     List<Product> findByStatusPembelian(int statusPembelian);
